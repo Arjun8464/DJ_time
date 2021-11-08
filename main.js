577ef9379a3d3ba49492fddb5a999e1a8e0173ab
Arjun8464 <71544080+Arjun8464@users.noreply.github.com>
2021-11-08 16:35:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristx=0;
+leftWristy=0;
+rightWrists=0;
+rightWristy=0;
 song = "";
 
 function preload(){
@@ -9,6 +13,8 @@ canvas = createCanvas(600 , 500);
 canvas.center();
 video = createCapture(VIDEO);
 video.hide();
+poseNet= ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses)
 }
 
 function draw(){
@@ -17,4 +23,19 @@ image(video , 0 , 0 , 600 , 500)
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded(){
+    console.log('Model Loaded!')
+}
+
+function gotPoses(results){
+    console.log(results);
+    leftWristx=results[0].pose.leftWrist.x;
+    leftWristy=results[0].pose.leftWrist.y;
+    rightWristx=results[0].pose.rightWrist.x;
+    rightWristy=results[0].pose.rightWrist.y;
+    console.log("leftWristx"+leftWristx+"rightWristx"+rightWristx+"leftWristy"+leftWristy+"rightWristy"+rightWristy);
 }
